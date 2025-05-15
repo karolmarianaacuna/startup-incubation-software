555bac9e150b0cc4670dc194517adabbfc8b7979
Karol Mariana Acuña Barrera <165958325+karolmarianaacuna@users.noreply.github.com>
2025-05-15 11:16:17 -0500

    cambios del 15/05/2025

--- src/main/java/com/codewithteam/proyecto_integrador/Controllers/indexController.java
+++ src/main/java/com/codewithteam/proyecto_integrador/Controllers/indexController.java
@@ -1,7 +1,9 @@
 package com.codewithteam.proyecto_integrador.Controllers;
 
 import com.codewithteam.proyecto_integrador.Entities.NoticiaEntity;
+import com.codewithteam.proyecto_integrador.Entities.StartupEntity;
 import com.codewithteam.proyecto_integrador.Models.Service.NoticiaService;
+import com.codewithteam.proyecto_integrador.Models.Service.StartupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,15 +15,43 @@ import java.util.List;
 public class indexController {
     @Autowired
     private NoticiaService noticiasService;
+    @Autowired
+    private StartupService startupService;
 
     @GetMapping("/blogNoticias")
-    public String blogNoticias(Model model){
-        List<NoticiaEntity>lista= noticiasService.findAll();
+    public String blogNoticias(Model model) {
+        List<NoticiaEntity> lista = noticiasService.findAll();
         model.addAttribute("noticias", lista);
-    //este es el nombre del html
+        //este es el nombre del html
         return "/noticias/blogNoticias";
-
     }
 
 
+    @GetMapping("/pruebaRegistrer")
+    public String registro() {
+        // Este es el nombre del html
+        return "/registroInicio/registro";
+    }
+
+    @GetMapping("/pruebaLogin")
+    public String Login() {
+        // Este es el nombre del html
+        return "/registroInicio/login";
+    }
+
+    @GetMapping("/pruebaFundadores")
+    public String Fundador() {
+        // Este es el nombre del html
+        return "/fundadores/fundadores";
+    }
+
+    @GetMapping("/pruebaStartups")
+    public String Startups(Model model) {
+
+        List<StartupEntity> startups = startupService.findAll();
+        model.addAttribute("startups", startups);
+        return "/startups/startups";
+    }
 }
+
+
