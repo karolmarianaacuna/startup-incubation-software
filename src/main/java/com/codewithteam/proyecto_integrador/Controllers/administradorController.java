5e19751d92062054c60e8a61beb21b27f8689c53
Karol Mariana Acuña Barrera <165958325+karolmarianaacuna@users.noreply.github.com>
2025-05-28 20:32:41 -0500

    Changes by alejo

--- src/main/java/com/codewithteam/proyecto_integrador/Controllers/administradorController.java
+++ src/main/java/com/codewithteam/proyecto_integrador/Controllers/administradorController.java
@@ -1,8 +1,12 @@
 package com.codewithteam.proyecto_integrador.Controllers;
 
 import com.codewithteam.proyecto_integrador.Entities.ConvocatoriaEntity;
+import com.codewithteam.proyecto_integrador.Entities.StartupEntity;
+import com.codewithteam.proyecto_integrador.Entities.UsuarioEntity;
 import com.codewithteam.proyecto_integrador.Models.Service.ConvocatoriaService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.codewithteam.proyecto_integrador.Models.Service.StartupService;
+import com.codewithteam.proyecto_integrador.Models.Service.UsuarioService;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +17,16 @@ import java.util.List;
 public class administradorController {
 
     private final ConvocatoriaService convocatoriaService;
+    private final UsuarioService usuarioService;
+    private final StartupService startupService;
 
-    public administradorController(ConvocatoriaService convocatoriaService) {
+    public administradorController(ConvocatoriaService convocatoriaService, UsuarioService usuarioService, StartupService startupService) {
         this.convocatoriaService = convocatoriaService;
+        this.usuarioService = usuarioService;
+        this.startupService =  startupService;
     }
+
+
 
     @GetMapping("/pruebaAdministrador")
     public String administrador(Model model) {
@@ -24,5 +34,21 @@ public class administradorController {
         model.addAttribute("convocatorias",lista );
         return "/administrador/administradorVista";
 
+    }
+
+    // Redirige a vista para gestionar usuarios
+    @GetMapping("/gestionarUsuarios")
+    public String gestionarUsuarios(Model model) {
+        List<UsuarioEntity> listaUsu = usuarioService.findAll();
+        model.addAttribute("usuario",listaUsu );
+        return "/administrador/gestionarUsuarios";
+    }
+
+    // Redirige a vista para asignar mentorias
+    @GetMapping("/asignarMentorias")
+    public String asignarMentorias(Model model) {
+        List<StartupEntity> listaStar = startupService.findAll();
+        model.addAttribute("proyecto",listaStar );
+        return "/administrador/asignarMentorias";
     }
 }
